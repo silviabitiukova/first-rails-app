f193ccd757163c9bad851ee753803ec5158b111f
silvia bitiukova <silvia.bitiukova@gmail.com>
2021-02-15 15:49:41 +0100

    fixed style

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/ask", to: "questions#ask"
+  get "/ask", to: "questions#ask", as: :ask
   get "/answer", to: "questions#answer"
 end
-
-
-# def coach_answer(your_message)
-#   if your_message.downcase == "i am going to work right now!"
-#     ""
-#   elsif your_message.end_with?("?")
-#     "Silly question, get dressed and go to work!"
-#   else
-#     "I don't care, get dressed and go to work!"
-#   end
-# end
-
-# def coach_answer_enhanced(your_message)
-#   answer = coach_answer(your_message)
-#   if answer != ""
-#     if your_message.upcase == your_message
-#       "I can feel your motivation! #{answer}"
-#     else
-#       answer
-#     end
-#   else
-#     ""
-#   end
-# end
